6d4dbef1140467c645f947e3c55646306d0b2431
unknown <madjideibdaien@gmail.com>
2024-02-12 20:58:34 +0100

    test git commit 001

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ db= SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer ,primary_key = True)
-    content = db.Column(db.String(200) , nullable=False)
+    content = db.Column(db.String(203) , nullable=False)
     completed = db.Column(db.Integer , default = 0)
 
 
